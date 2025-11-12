245ce8466da96f8849a5914c241e3db87006f789
divyapatel <divya.patel@wdc.com>
2025-11-12 12:07:40 +0530

    test husky

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
+  const x=;
   console.log("Home page rendered");
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
