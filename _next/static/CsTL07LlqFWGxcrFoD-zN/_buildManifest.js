1d0eb5e2e28ee5dc2599f01f94a82bb0273404bd
divya-patel-git <divyapatel20051990@gmail.com>
2025-11-12 16:34:07 +0000

    Deploying to gh-pages from @ divya-patel-git/demo-ci-cd-app@e72731a223c5437f5e39aef3365ea37da868721a 🚀

--- _next/static/CsTL07LlqFWGxcrFoD-zN/_buildManifest.js
+++ _next/static/CsTL07LlqFWGxcrFoD-zN/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/demo-ci-cd-app//_next/:path+",
+        "destination": "/demo-ci-cd-app/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
