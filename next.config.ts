579a969ec26d9792cc1ad58a7367bb1c5e735c07
divyapatel <divya.patel@wdc.com>
2025-11-12 15:54:05 +0530

    Add Github action to perform CD for GithubPages.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    // This is needed for GitHub Pages
+  output: 'export',
+
+  // 👇 Replace 'nextjs14-ci-cd' with your repo name
+  basePath: '/demo-ci-cd-app',
+
+  // 👇 Also set the same prefix for static assets
+  assetPrefix: '/demo-ci-cd-app/',
   /* config options here */
 };
 
