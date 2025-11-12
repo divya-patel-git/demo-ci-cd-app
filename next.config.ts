17c640e2597efc93d4bc4063a9b104910db5d57f
divyapatel <divya.patel@wdc.com>
2025-11-12 21:58:13 +0530

    Add Github action to perform CD for GithubPages Issue fix.

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,11 @@ const isProd = process.env.NODE_ENV === 'production' ?true : false;
 const nextConfig: NextConfig = {
    output: 'export',
    //for dev purpose only
- basePath: '',
- assetPrefix: '',
+//  basePath: '',
+//  assetPrefix: '',
   // 👇 Change this to your repo name (or folder name)
-  // basePath: isProd ? '/demo-ci-cd-app' : '',
-  // assetPrefix: isProd ? '/demo-ci-cd-app/' : '',
+  basePath: isProd ? '/demo-ci-cd-app' : '',
+  assetPrefix: isProd ? '/demo-ci-cd-app/' : '',
   images: {
     unoptimized: true, // Required since export disables optimization
   },
