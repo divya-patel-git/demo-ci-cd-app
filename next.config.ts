e72731a223c5437f5e39aef3365ea37da868721a
divyapatel <divya.patel@wdc.com>
2025-11-12 22:03:20 +0530

    Add Github action to perform CD for GithubPages Issue fix.

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,29 @@
-import type { NextConfig } from "next";
-const isProd = process.env.NODE_ENV === 'production' ?true : false;
-const nextConfig: NextConfig = {
-   output: 'export',
-   //for dev purpose only
-//  basePath: '',
-//  assetPrefix: '',
-  // 👇 Change this to your repo name (or folder name)
-  basePath: isProd ? '/demo-ci-cd-app' : '',
-  assetPrefix: isProd ? '/demo-ci-cd-app/' : '',
-  images: {
-    unoptimized: true, // Required since export disables optimization
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/demo-ci-cd-app',
+  assetPrefix: '/demo-ci-cd-app/',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
+
+// import type { NextConfig } from "next";
+// const isProd = process.env.NODE_ENV === 'production' ?true : false;
+// const nextConfig: NextConfig = {
+//    output: 'export',
+//    //for dev purpose only
+// //  basePath: '',
+// //  assetPrefix: '',
+//   // 👇 Change this to your repo name (or folder name)
+//   basePath: isProd ? '/demo-ci-cd-app' : '',
+//   assetPrefix: isProd ? '/demo-ci-cd-app/' : '',
+//   images: {
+//     unoptimized: true, // Required since export disables optimization
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+// };
+
+// export default nextConfig;
